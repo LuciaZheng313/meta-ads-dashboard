187957dd9934db4f0fba43388f88d223a8e807a9
luciaz <luckyzheng313@gmail.com>
2026-06-16 18:32:34 +0800

    “debug”

--- app.py
+++ app.py
@@ -56,18 +56,22 @@ st.markdown("""
         border-color: #5b7ed8 !important;
     }
 
-    /* Radio buttons */
+    /* Radio buttons - outer circle */
     .stRadio > div[role="radiogroup"] > label > div:first-child {
         background-color: white !important;
         border-color: #d0d0d0 !important;
     }
+    /* Radio button when checked - outer circle becomes blue */
     .stRadio > div[role="radiogroup"] > label[data-checked="true"] > div:first-child {
-        background-color: #5b7ed8 !important;
+        background-color: white !important;
         border-color: #5b7ed8 !important;
     }
-    /* Radio button inner circle when checked - make it black */
-    .stRadio > div[role="radiogroup"] > label[data-checked="true"] > div:first-child > div {
+    /* Radio button inner dot when checked - make it black */
+    .stRadio > div[role="radiogroup"] > label[data-checked="true"] > div:first-child::before {
         background-color: #000000 !important;
+    }
+    .stRadio > div[role="radiogroup"] > label[data-checked="true"] svg circle {
+        fill: #000000 !important;
     }
 </style>
 """, unsafe_allow_html=True)
